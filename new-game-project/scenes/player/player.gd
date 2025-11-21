14147df4b407684babb1cdebbd09c431af27377f
Piter <61157319+PiterDev@users.noreply.github.com>
2025-11-21 21:49:52 +0100

    Make basic attack logic

--- new-game-project/scenes/player/player.gd
+++ new-game-project/scenes/player/player.gd
@@ -12,3 +12,14 @@ func _input(event: InputEvent) -> void:
 		rotate_y(-event.relative.x * mouse_sensitivity)
 		$Camera3D.rotate_x(-event.relative.y * mouse_sensitivity)
 		$Camera3D.rotation.x = clampf($Camera3D.rotation.x, -deg_to_rad(70), deg_to_rad(70))
+	if event.is_action_pressed("attack_left") or event.is_action_pressed("attack_right"):
+		$AnimationPlayer.seek(0)
+		$AnimationPlayer.play("camera_thump")
+
+func attack() -> void:
+	$Camera3D/AttackRaycast.force_raycast_update()
+	var collider := $Camera3D/AttackRaycast.get_collider() as Object
+	#if collider is Enemy:
+		## Calculate precision, add score
+	#else:
+		## Subtract from score
