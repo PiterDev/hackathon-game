22f03fef56ff8ffd6a2f799dd7ae5b1fa191426e
Piter <61157319+PiterDev@users.noreply.github.com>
2025-11-21 20:13:48 +0100

    Create player controlled

---
+++ new-game-project/scenes/player/player.gd
@@ -0,0 +1,14 @@
+extends CharacterBody3D
+# Adapted from https://kidscancode.org/godot_recipes/4.x/3d/basic_fps/index.html
+
+var speed := 5
+var mouse_sensitivity := 0.001
+
+func _ready() -> void:
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+
+func _input(event: InputEvent) -> void:
+	if event is InputEventMouseMotion and Input.mouse_mode == Input.MOUSE_MODE_CAPTURED:
+		rotate_y(-event.relative.x * mouse_sensitivity)
+		$Camera3D.rotate_x(-event.relative.y * mouse_sensitivity)
+		$Camera3D.rotation.x = clampf($Camera3D.rotation.x, -deg_to_rad(70), deg_to_rad(70))
