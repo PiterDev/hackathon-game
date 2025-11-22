40e2230f4719ef1e9562c46017cda1dd13cccf42
Piter <61157319+PiterDev@users.noreply.github.com>
2025-11-22 09:25:51 +0100

    Add fullscren button

--- new-game-project/scenes/main_game_scene/game.gd
+++ new-game-project/scenes/main_game_scene/game.gd
@@ -14,3 +14,9 @@ func load_lvl() -> void:
 func _next_lvl() -> void:
 	curr_lvl += 1
 	load_lvl()
+
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("fullscreen"):
+		var mode := DisplayServer.window_get_mode()
+		var is_window: bool = mode != DisplayServer.WINDOW_MODE_FULLSCREEN
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN if is_window else DisplayServer.WINDOW_MODE_WINDOWED)
