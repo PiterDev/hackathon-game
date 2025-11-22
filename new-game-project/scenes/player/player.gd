f5fc647477d9bfc2fbb99defb94324a5dd52c179
Piter <61157319+PiterDev@users.noreply.github.com>
2025-11-22 02:13:04 +0100

    Add enemy ragdoll

--- new-game-project/scenes/player/player.gd
+++ new-game-project/scenes/player/player.gd
@@ -14,8 +14,10 @@ func _ready() -> void:
 func _input(event: InputEvent) -> void:
 	if event is InputEventMouseMotion and Input.mouse_mode == Input.MOUSE_MODE_CAPTURED:
 		rotate_y(-event.relative.x * mouse_sensitivity)
+		
 		$Camera3D.rotate_x(-event.relative.y * mouse_sensitivity)
 		$Camera3D.rotation.x = clampf($Camera3D.rotation.x, -deg_to_rad(70), deg_to_rad(70))
+		
 	if event.is_action_pressed("Action"):
 		rig.animation_player.seek(0)
 		$AnimationPlayer.seek(0)
@@ -32,6 +34,7 @@ func attack() -> void:
 	var collider := $Camera3D/AttackRaycast.get_collider() as Object
 	if collider and collider.is_in_group("Enemy") and collider.own_type == Enemy.EnemyType.BUMPER:
 		# Calculate precision, add score
+		$AttackSound.play(0.0)
 		var enemy := collider as CharacterBody3D
 		enemy.die()
 	else:
